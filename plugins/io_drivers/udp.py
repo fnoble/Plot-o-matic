7a54d449068a9054300319c44ef1ece1d34c5504
Fergus Noble <fergusnoble@gmail.com>
2010-07-26 23:07:55 -0700

    Added address filtering to UDP driver and fixed rebinding the socket when changing port.

--- plugins/io_drivers/udp.py
+++ plugins/io_drivers/udp.py
@@ -1,4 +1,4 @@
-from enthought.traits.api import Str, Float, File, Int, on_trait_change
+from enthought.traits.api import Str, Float, File, Int, Bool, on_trait_change
 from enthought.traits.ui.api import View, Item
 
 import time
@@ -13,15 +13,19 @@ class UDPDriver(IODriver):
   
   name = Str('UDP Driver')
   view = View(
-    Item(name = 'port', label='Port'),
-    Item(name = 'ip', label='IP Address'),
+    Item(name='port', label='Port'),
+    Item(name='filter_by_addr', label='Filter by remote address'),
+    Item(name='filter_addr', label='Filter address'),    
+    Item(name='ip', label='IP Address to bind'),
     title='UDP input driver'
   )
   
   sock = socket.socket()
 
-  port = Int(2222)
+  port = Int(21212)
   ip = Str('127.0.0.1')
+  filter_by_addr = Bool(False)
+  filter_addr = Str('')
   
   def open(self):
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -33,16 +37,17 @@ class UDPDriver(IODriver):
   
   def receive(self):
     try:
-      data, addr = self.sock.recvfrom(102400) # buffer size is 10kb
+      (data, (addr, port)) = self.sock.recvfrom(102400) # buffer size is 10kb
+      print "UDP packet from '%s' size %u bytes" % (addr, len(data))
+      if self.filter_by_addr and addr != self.filter_addr:
+        return None
     except socket.timeout:
       return None
-    
-    #print "Received UDP message from %s, size %d bytes" % (addr[0], len(data))
     return data
 
   def rebind_socket(self):
     self.sock.close()
-    self.sock.bind((self.ip, self.port))
+    self.open()
 
   @on_trait_change('port')
   def change_port(self, old_port, new_port):
