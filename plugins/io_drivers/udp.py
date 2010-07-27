02930c3299c08898adc74c724d96735add99c683
Fergus Noble <fergusnoble@gmail.com>
2010-07-27 00:53:39 -0700

    Set default IP address to bind to in UDP driver to 0.0.0.0 so we can receive broadcast packets.

--- plugins/io_drivers/udp.py
+++ plugins/io_drivers/udp.py
@@ -16,16 +16,21 @@ class UDPDriver(IODriver):
     Item(name='port', label='Port'),
     Item(name='filter_by_addr', label='Filter by remote address'),
     Item(name='filter_addr', label='Filter address'),    
-    Item(name='ip', label='IP Address to bind'),
+    Item(name='ip', label='IP address to bind'),
+    Item(label='(use 0.0.0.0 to bind \nto all interfaces)'),
     title='UDP input driver'
   )
   
   sock = socket.socket()
 
   port = Int(21212)
-  ip = Str('127.0.0.1')
+  ip = Str('0.0.0.0')
   filter_by_addr = Bool(False)
   filter_addr = Str('')
+  
+  #def _ip_default(self):
+  #  print "UDP driver: my hostname '%s' (ip %s)" % (socket.gethostname(), socket.gethostbyname(socket.gethostname()))
+  #  return socket.gethostbyname(socket.gethostname())
   
   def open(self):
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -38,7 +43,7 @@ class UDPDriver(IODriver):
   def receive(self):
     try:
       (data, (addr, port)) = self.sock.recvfrom(102400) # buffer size is 10kb
-      print "UDP packet from '%s' size %u bytes" % (addr, len(data))
+      print "UDP driver: packet from '%s' size %u bytes" % (addr, len(data))
       if self.filter_by_addr and addr != self.filter_addr:
         return None
     except socket.timeout:
