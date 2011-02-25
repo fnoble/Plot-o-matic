137956a51def25098b1a7d1e5b4678795d2c927a
Greg Horn <gregmainland@gmail.com>
2011-02-25 12:26:45 -0800

    default save/open directory is saved_sessions

--- plot-o-matic.py
+++ plot-o-matic.py
@@ -33,7 +33,7 @@ if PROFILE:
   import yappi
   yappi.start(PROFILE_BUILTINS)
 
-def open_file(filter = '', dir = './', file_name = ''):
+def open_file(filter = '', dir = './saved_sessions', file_name = ''):
   dialog = wx.FileDialog(None, wildcard = filter, defaultFile = file_name, defaultDir = dir, style = wx.OPEN|wx.FILE_MUST_EXIST)
   file_name = ''
   if dialog.ShowModal() == wx.ID_OK:
@@ -41,7 +41,7 @@ def open_file(filter = '', dir = './', file_name = ''):
   dialog.Destroy()
   return file_name
 
-def save_file(filter = '', dir = './', file_name = ''):
+def save_file(filter = '', dir = './saved_sessions', file_name = ''):
   dialog = wx.FileDialog(None, wildcard = filter, defaultFile = file_name, defaultDir = dir, style = wx.SAVE|wx.OVERWRITE_PROMPT)
   filename = ''
   if dialog.ShowModal() == wx.ID_OK:
