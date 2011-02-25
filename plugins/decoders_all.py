827a8bddcddf8860733fcb8209f7cde3ee627323
Fergus Noble <fergusnoble@gmail.com>
2011-02-25 23:04:17 +0000

    Ivy plugins disabled for now.

--- plugins/decoders_all.py
+++ plugins/decoders_all.py
@@ -7,4 +7,4 @@ from plugins.decoders.null_decoder import *
 from plugins.decoders.cstruct_decoder import *
 from plugins.decoders.simpleplot import *
 from plugins.decoders.jobysim_decoder import *
-from plugins.decoders.paparazzi_ivy_decoder import *
+#from plugins.decoders.paparazzi_ivy_decoder import *
